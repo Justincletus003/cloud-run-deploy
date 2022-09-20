4cea0058530138bbcf508034bc6e756505c4374a
Justin Cletus <90815503+Justincletus003@users.noreply.github.com>
2022-09-20 18:33:21 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+)
+
+func main() {
+    fmt.Print("Starting server")
+    http.HandleFunc("/", handler)
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+    log.Printf("listening on port %s", port)
+    if err = http.ListenAndServe(":"+port); err != nil {
+        log.Fatal(err)        
+    }    
+    
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+    name := os.Getenv("NAME")
+    if name == "" {
+        name = "World"
+    }
+    w.HeaderWriter(http.StatusOk)
+    fmt.Fprintf(w, "hello %s", name)
+}
