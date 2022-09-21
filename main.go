f93aba1eee8ddb49d99df313ef4da7de554ea538
Justin Cletus <justin.cletus@pantheon.io>
2022-09-21 17:52:45 +0530

    migration init function added

--- main.go
+++ main.go
@@ -2,16 +2,77 @@ package main
 
 import (
 	"database/sql"
+	"embed"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-    "github.com/golang-migrate/migrate/v4"
-    "github.com/golang-migrate/migrate/v4/database/mysql"
-    _ "github.com/golang-migrate/migrate/v4/source/file"
+	migrate "github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/mysql"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+	"github.com/spf13/cobra"
 )
+
+var fs embed.FS
+type User struct{
+    ID int `gorm:"primaryKey,autoIncrement"`
+    Firstname string
+    Lastname string
+}
+
+var rootCmd = &cobra.Command{
+    Use: "app",
+    Short: "this is go migrate example",
+}
+
+var migrateCmd = &cobra.Command{
+    Use: "migrate",
+    Short: "Run database migration",
+    
+    Run: func(cmd *cobra.Command, args []string)  {
+        user := os.Getenv("user")
+        if user == "" {
+            fmt.Errorf("user is not empty")
+            return
+        }
+
+        password := os.Getenv("password")
+        if password == "" {
+            fmt.Errorf("password is not empty")
+            return
+            
+        }
+
+        dbname := os.Getenv("dbname")
+        if dbname == "" {
+            fmt.Errorf("dbname is not empty")
+            return
+        }       
+
+        host := "/cloudsql/pantheon-lighthouse-poc:us-central1:lighthousedb"
+
+        dbURI := fmt.Sprintf("%s:%s@unix(/%s)/%s?parseTime=true", user, password, host, dbname)
+
+        d, err := iofs.New(fs, "migrations")
+        if err != nil {
+            log.Fatal(err)
+        }
+        m, err := migrate.NewWithSourceInstance(
+            "iofs", d, dbURI)
+        if err != nil {
+            panic(fmt.Sprintf("unable to connect database %v", err))
+        }
+        m.Up()
+        
+    },
+}
+
+func init() {    
+    rootCmd.AddCommand(migrateCmd)
+}
+
 
 func main() {
     fmt.Print("Starting server ")
@@ -80,24 +141,24 @@ func handler(w http.ResponseWriter, r *http.Request){
     // w.Write([]byte(strings.createTable.LastInsertId()))
 
     // w.Write([]byte("testing driver "))
-    dbDriver, err := mysql.WithInstance(db, &mysql.Config{})
-    if err != nil {
-        w.Write([]byte(err.Error()))
-        return
-    }
-    // w.Write([]byte("testing driver output"))
+    // dbDriver, err := mysql.WithInstance(db, &mysql.Config{})
+    // if err != nil {
+    //     w.Write([]byte(err.Error()))
+    //     return
+    // }
+    // // w.Write([]byte("testing driver output"))
 
-    m, err := migrate.NewWithDatabaseInstance(
-        "file:///migrations",
-        "mysql", 
-        dbDriver,
-    )
-    if err != nil {
-        w.Write([]byte(err.Error()))
-        return
-    }
+    // m, err := migrate.NewWithDatabaseInstance(
+    //     "file:///migrations",
+    //     "mysql", 
+    //     dbDriver,
+    // )
+    // if err != nil {
+    //     w.Write([]byte(err.Error()))
+    //     return
+    // }
     
-    m.Steps(2)
+    // m.Steps(2)
     // fmt.Println(dbDriver)
     w.WriteHeader(http.StatusOK)
     fmt.Fprintln(w, "database successfully connected")
