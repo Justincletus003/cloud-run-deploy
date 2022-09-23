18ccae48050b6378924f01cfb6bf402a833c18d2
Justin Cletus <justin.cletus@pantheon.io>
2022-09-23 16:29:39 +0530

    cat the password

--- workspace/env.sh
+++ workspace/env.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo ${_INSTANCE_CONNECTION_NAME}
-export DB_PASS=${DATABASE_PASS}
+echo ${DATABASE_PASS} > DB_PASS
 echo ${CRED_FILE} > cloud-sa.json
 
-/workspace/cloud_sql_proxy -dir=/workspace -instances=${_INSTANCE_CONNECTION_NAME} -credential_file=cloud-sa.json & sleep 2
+
+
 
 # /workspace/cloud_sql_proxy -dir=/workspace -instances=${_INSTANCE_CONNECTION_NAME} -credential_file=${CRED_FILE} & sleep 2
