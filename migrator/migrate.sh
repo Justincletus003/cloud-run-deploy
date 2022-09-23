24b11b2c821e8f421b0285bd3e5b1faa0359ec3c
Justin Cletus <justin.cletus@pantheon.io>
2022-09-23 12:23:28 +0530

    proxy connection testing

--- migrator/migrate.sh
+++ migrator/migrate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo ${DATABASE_PASS}
-docker --version
+# echo ${DATABASE_PASS}
+host="/cloudsql/pantheon-lighthouse-poc:us-central1:lighthousedb"
+dbURL=mysql://${DATABASE_USER}:${DATABASE_PASS}@unix
 
+dbURI := fmt.Sprintf("%s:%s@unix(/%s)/%s?parseTime=true&multiStatements=true", user, password, host, dbname)
 docker run  --network host migrator -path=/migrations/ -database "mysql://${DATABASE_USER}:${DATABASE_PASS}@localhost:3306/test_db?sslmode=disable" up
