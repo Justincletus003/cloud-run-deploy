299a82fa5355b2123b937b733341dd3cb8321cb8
Justin Cletus <justin.cletus@pantheon.io>
2022-09-23 16:36:58 +0530

    pwd test

--- workspace/env.sh
+++ workspace/env.sh
@@ -3,7 +3,3 @@ echo ${_INSTANCE_CONNECTION_NAME}
 echo ${DATABASE_PASS} > DB_PASS
 echo ${CRED_FILE} > cloud-sa.json
 
-
-
-
-# /workspace/cloud_sql_proxy -dir=/workspace -instances=${_INSTANCE_CONNECTION_NAME} -credential_file=${CRED_FILE} & sleep 2
