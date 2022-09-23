9a243d47530a9c2aac3fb3840aa6863802172bf4
Justin Cletus <justin.cletus@pantheon.io>
2022-09-23 15:43:45 +0530

    export key file

--- workspace/env.sh
+++ workspace/env.sh
@@ -2,4 +2,6 @@
 # echo ${CRED_FILE}
 echo ${_INSTANCE_CONNECTION_NAME}
 echo ${INSTANCE_CONNECTION_NAME}
-/workspace/cloud_sql_proxy -dir=/workspace -instances=${_INSTANCE_CONNECTION_NAME} -credential_file=${CRED_FILE} & sleep 2
+echo ${CRED_FILE} > cloud-sa.json
+
+# /workspace/cloud_sql_proxy -dir=/workspace -instances=${_INSTANCE_CONNECTION_NAME} -credential_file=${CRED_FILE} & sleep 2
