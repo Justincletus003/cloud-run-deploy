0f7c03132ed1426645544597021494a94e2cb7c4
Justin Cletus <justin.cletus@pantheon.io>
2022-09-26 12:09:54 +0530

    install migrate

--- workspace/env.sh
+++ workspace/env.sh
@@ -2,5 +2,5 @@
 echo ${_INSTANCE_CONNECTION_NAME}
 echo ${DATABASE_PASS} > DB_PASS
 echo ${CRED_FILE} > cloud-sa.json
-curl --version
-tar --version
+curl -L https://github.com/golang-migrate/migrate/releases/download/$version/migrate.$platform-amd64.tar.gz | tar xvz
+migrate -version
