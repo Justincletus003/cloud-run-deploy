264ad2882d8c3771f779a16134d8dc7ae3357cc3
Justin Cletus <justin.cletus@pantheon.io>
2022-09-26 15:16:49 +0530

    t3

--- workspace/env.sh
+++ workspace/env.sh
@@ -2,5 +2,3 @@
 echo ${_INSTANCE_CONNECTION_NAME}
 echo ${DATABASE_PASS} > DB_PASS
 echo ${CRED_FILE} > cloud-sa.json
-curl -L https://github.com/golang-migrate/migrate/releases/download/$version/migrate.$platform-amd64.tar.gz | tar xvz
-migrate -version
