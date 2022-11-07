c462a274d0c918e5f1bd7cad96637ff964e46094
Justin Cletus <justin.cletus@pantheon.io>
2022-11-07 19:43:33 +0530

    t1

--- workspace/env.sh
+++ workspace/env.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-echo "hello";
-echo ${_ENV_NAME}---;
-echo "world";
-
-if [ ${_ENV_NAME}="prod" ]
-then
-    echo "prod env";
-else
-    echo "dev";
-fi
-
 echo ${_INSTANCE_CONNECTION_NAME}
 echo ${DATABASE_PASS} > DB_PASS
 echo ${CRED_FILE} > cloud-sa.json
